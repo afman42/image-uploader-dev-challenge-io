a41c33ef9c838ca0bc3a7920cee350642d08783d
afman42 <afifarman50@gmail.com>
2023-05-26 11:02:53 +0700

    change font size

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="flex flex flex-col">
-        <h1 class="text-2xl font-poppins font-medium">404</h1>
-        <RouterLink to="/" class="px-4 py-2 rounded-lg bg-blue-500 text-[#FFF] hover:text-transparent">Back</RouterLink>
+        <h1 style="font-size: 250px;" class="font-poppins font-medium">404</h1>
+        <div class="flex justify-center w-full">
+            <RouterLink to="/" class="w-28 h-8 flex items-center justify-center rounded-lg bg-blue-500 text-[#FFF] hover:text-black hover hover:border-2 hover:border-blue-500 hover:bg-[#FFF]">Back</RouterLink>
+        </div>
     </div>
 </template>
